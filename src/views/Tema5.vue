8d8f37ec569f3ab4401b007cca042ac8ca2565e2
elrilacho <camilobr17@gmail.com>
2023-03-29 08:51:14 -0500

    Componente listo

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -19,7 +19,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/44zfIWn4Hu4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
     h3(data-aos="fade-right") Ventajas y desventajas de las máquinas herramientas CNC
 
